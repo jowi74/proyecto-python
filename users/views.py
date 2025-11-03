da526109059ec07da3a110d8fae19fef6fac6939
jowi74 <yoel.zamora@colegiomiralmonte.es>
2025-11-03 12:30:06 +0100

    creacion de la pagina login y logout

--- users/views.py
+++ users/views.py
@@ -1,13 +1,34 @@
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("apppage")   # redirige donde quieras
+            user = form.save()
+            login(request, user)
+            return redirect('apppage')
     else:
         form = UserCreationForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('apppage')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+
+def logout_user(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('apppage')
+
